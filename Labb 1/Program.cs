fd8899b1ba00cb3579679ef7463a2d8057543389
Zyphyreon <112382616+Zyphyreon@users.noreply.github.com>
2022-09-14 13:34:24 +0200

    Add project files.

---
+++ Labb 1/Program.cs
@@ -0,0 +1,40 @@
+﻿//string input = "29535123p48723487597645723645";
+string tal = string.Empty;
+long output = 0;
+int i = 0;
+int j = 1;
+
+Console.Write("Ange ett antal siffror och bokstäver: ");
+string input = Console.ReadLine();
+for (i = 0; i < input.Length - 1; i++)
+{
+
+    for (j = i + 1; j < input.Length; j++)
+    {
+
+        if (Char.IsDigit(input[j]))
+        {
+
+
+            if (input[j] == input[i])
+            {
+                Console.Write(input.Substring(0, i));
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(input.Substring(i, j - i + 1));
+                Console.ForegroundColor = ConsoleColor.White;
+                output += Convert.ToInt64(input.Substring(i, j - i + 1));
+                Console.WriteLine(input.Substring(j + 1));
+                break;
+            }
+
+        }
+        else
+        {
+
+            break;
+        }
+
+    }
+
+}
+Console.WriteLine("\n" + "Total = " + output);
